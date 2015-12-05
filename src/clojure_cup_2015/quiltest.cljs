a9c995300100f330e8dd30252b55454e9dcf3afb
Arne Brasseur <arne@arnebrasseur.net>
2015-12-05 15:40:27 +0100

    More examples

--- src/clojure_cup_2015/quiltest.cljs
+++ src/clojure_cup_2015/quiltest.cljs
@@ -1,18 +1,344 @@
 (ns clojure-cup-2015.quiltest
-  (:require [quil.core :as q :refer-macros [with-translation defsketch]]
+  (:require [quil.core :as q
+             :refer [
+;;;; COLOR ;;;;
+
+;; Creating & Reading
+alpha
+blend-color
+blue
+brightness
+color
+color-mode
+current-fill
+current-stroke
+green
+hue
+lerp-color
+red
+saturation
+
+;; Setting
+background
+background-float
+background-image
+background-int
+fill
+fill-float
+fill-int
+no-fill
+no-stroke
+stroke
+stroke-float
+stroke-int
+
+;; Utility Macros
+;; with-fill
+;; with-stroke
+
+;;;; TYPOGRAPHY ;;;;
+
+;; Attributes
+text-align
+text-leading
+text-mode
+text-size
+text-width
+
+;; Loading & Displaying
+available-fonts
+create-font
+load-font
+text
+text-char
+text-font
+text-num
+
+;; Metrics
+text-ascent
+text-descent
+
+;;;; IMAGE ;;;;
+create-image
+resize
+
+;; Loading & Displaying
+image
+image-mode
+load-image
+no-tint
+request-image
+tint
+tint-float
+tint-int
+
+;; Pixels
+blend
+copy
+display-filter
+get-pixel
+image-filter
+pixels
+set-image
+set-pixel
+update-pixels
+
+;; Rendering
+create-graphics
+
+;;;; TRANSFORM ;;;;
+apply-matrix
+pop-matrix
+print-matrix
+push-matrix
+reset-matrix
+rotate
+rotate-x
+rotate-y
+rotate-z
+scale
+shear-x
+shear-y
+translate
+
+;; Utility Macros
+;; with-rotation
+;; with-translation
+
+;;;; RENDERING ;;;;
+hint
+;; with-graphics
+
+;; Shaders
+load-shader
+
+;;;; MATH ;;;;
+
+;; Calculation
+abs
+ceil
+constrain
+dist
+exp
+floor
+lerp
+log
+mag
+map-range
+norm
+pow
+round
+sq
+sqrt
+
+;; Random
+noise
+noise-detail
+noise-seed
+random
+random-gaussian
+random-seed
+
+;; Trigonometry
+acos
+asin
+atan
+atan2
+cos
+degrees
+radians
+sin
+tan
+
+;;;; DATA ;;;;
+
+;; Conversion
+binary
+hex
+unbinary
+unhex
+
+;;;; STATE ;;;;
+set-state!
+state
+state-atom
+
+;;;; SHAPE ;;;;
+
+;; 2D Primitives
+arc
+ellipse
+line
+point
+quad
+rect
+triangle
+
+;; 3D Primitives
+box
+sphere
+sphere-detail
+
+;; Attributes
+ellipse-mode
+no-smooth
+rect-mode
+smooth
+stroke-cap
+stroke-join
+stroke-weight
+
+;; Curves
+bezier
+bezier-detail
+bezier-point
+bezier-tangent
+curve
+curve-detail
+curve-point
+curve-tangent
+curve-tightness
+
+;; Loading & Displaying
+load-shape
+shape
+shape-mode
+
+;; Vertex
+begin-contour
+begin-shape
+bezier-vertex
+curve-vertex
+end-contour
+end-shape
+quadratic-vertex
+texture
+texture-mode
+vertex
+
+;;;; LIGHTS, CAMERA ;;;;
+
+;; Camera
+begin-camera
+camera
+end-camera
+frustum
+ortho
+perspective
+print-camera
+print-projection
+
+;; Coordinates
+model-x
+model-y
+model-z
+screen-x
+screen-y
+screen-z
+
+;; Lights
+ambient-light
+directional-light
+light-falloff
+light-specular
+lights
+no-lights
+normal
+point-light
+spot-light
+
+;; Material Properties
+ambient
+ambient-float
+ambient-int
+emissive
+emissive-float
+emissive-int
+shininess
+specular
+
+;;;; ENVIRONMENT ;;;;
+current-frame-rate
+current-graphics
+cursor
+cursor-image
+;;defsketch
+focused
+frame-count
+frame-rate
+height
+no-cursor
+sketch
+target-frame-rate
+width
+
+;;;; INPUT ;;;;
+
+;; Keyboard
+key-as-keyword
+key-code
+key-coded?
+key-pressed?
+raw-key
+
+;; Mouse
+mouse-button
+mouse-pressed?
+mouse-x
+mouse-y
+pmouse-x
+pmouse-y
+
+;; Time & Date
+day
+hour
+millis
+minute
+month
+seconds
+year
+
+;;;; OUTPUT ;;;;
+
+;; Files
+end-raw
+
+;; Image
+save
+save-frame
+
+;;;; STRUCTURE ;;;;
+delay-frame
+exit
+no-loop
+pop-style
+push-style
+redraw
+start-loop
+
+;;;; MIDDLEWARE ;;;;
+;; fun-mode
+;; navigation-2d
+;; navigation-3d
+]
+             :refer-macros [with-translation defsketch]]
             [quil.middleware :as m]
 
             ))
 
 (enable-console-print!)
 
-(println "Edits to this text should show up in your developer foobaz")
+(defn create-canvas [id]
+  (let [el (.createElement js/document "canvas")]
+    (aset el "id" (str id))
+    (aset el "style" "clear: both;")
+    (.appendChild (.-body js/document) el)))
 
 
 ; define function which draws spiral
 (defn draw []
   ; make background white
-  (q/background 255)
+  (background 255)
 
   ; move origin point to centre of the sketch
   ; by default origin is in the left top corner
@@ -20,17 +346,68 @@
    ; parameter t goes 0, 0.01, 0.02, ..., 99.99, 100
    (doseq [t (range 0 100 0.01)]
      ; draw a point with x = t * sin(t) and y = t * cos(t)
-     (q/point (* t (q/sin t))
+     (point (* t (q/sin t))
               (* t (q/cos t))))))
 
-;; ; run sketch
-;; (defsketch trigonometry
-;;   :host "canvas-id"
-;;   :size [300 300]
-;;   :draw draw)
-
-; run sketch
-(defsketch shapes-and-colors
-  :host "canvas-id"
+(create-canvas "trigonometry")
+(defsketch trigonometry
+  :host "trigonometry"
   :size [300 300]
-  :draw (fn []))
+  :draw draw)
+
+
+
+(create-canvas "shapes")
+(defsketch shapes
+  :host "shapes"
+  :size [300 300]
+  :draw (fn []
+          ;;            x1  y1  x2 y2
+          (q/rect       50  50  100 100)
+
+          ;;             x   y  width  height
+          (q/ellipse    90 250     90     70)
+
+          ;;            x1  y1    x2 y2     x3  y3
+          (q/triangle  200  20   175 75    250  75)
+
+          ;;            x1   y1    x2  y2
+          (q/line      230  150   290 220)
+          (q/line      230  220   290 150)
+          ))
+
+(defn fill-orange []
+  (q/fill 252 90 44))
+
+(defn fill-pink []
+  (q/fill 241 104 176))
+
+(defn fill-blue []
+  (q/fill 45 119 242))
+
+(defn black-stroke []
+  (q/stroke 0 0 0))
+
+(create-canvas "color-and-shape")
+(defsketch shapes
+  :host "color-and-shape"
+  :size [300 300]
+  :draw (fn []
+          (fill-orange)
+          ;;            x1  y1  x2 y2
+          (q/rect       50  50  100 100)
+
+          (fill-blue)
+          (q/no-stroke)
+          ;;             x   y  width  height
+          (q/ellipse    90 250     90     70)
+
+          (fill-pink)
+          (black-stroke)
+          ;;            x1  y1    x2 y2     x3  y3
+          (q/triangle  200  20   175 75    250  75)
+
+          ;;            x1   y1    x2  y2
+          (q/line      230  150   290 220)
+          (q/line      230  220   290 150)
+          ))
