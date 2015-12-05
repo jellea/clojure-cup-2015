247cd63e16c5faf7b0c15332b9371198616b3619
Arne Brasseur <arne@arnebrasseur.net>
2015-12-05 17:43:05 +0100

    Remove circular require

--- src/clojure_cup_2015/content.cljs
+++ src/clojure_cup_2015/content.cljs
@@ -1,17 +1,10 @@
-(ns clojure-cup-2015.content
-  #_(:require [clojure-cup-2015.core :refer [editor]]))
+(ns clojure-cup-2015.content)
 
-#_(defn quil-editor [id default-code]
-    [:div
-     [editor default-code]
-     [:canvas {:id id}]])
-
-#_(defn chapter-1 []
-    [:div
-     [:p "Quil lets you do visual programming. You can make drawings and animations, and give instant feedback to user interaction."]
-     [:p "A Quil program is called a sketch. Here's a sketch of three pink triangles on a blue background."]
-     [quil-editor
-      "pink-triangles" "(defn draw-pink-triangles []
+(defn chapter-1 []
+  [:div
+   [:p "Quil lets you do visual programming. You can make drawings and animations, and give instant feedback to user interaction."]
+   [:p "A Quil program is called a sketch. Here's a sketch of three pink triangles on a blue background."]
+   [:quil-code "pink-triangles" "(defn draw-pink-triangles []
   (no-stroke)
 
   (fill 244 213 221) ;; #f4d5dd
@@ -23,8 +16,7 @@
   (fill 232 181 188) ;; #e8b5bc
   (triangle 150 40, 227 50, 90 170))
 
-#_(defsketch pink-triangles
+(defsketch pink-triangles
   :host \"pink-triangles\"
   :size [300 300]
-  :draw draw-pink-triangles)"]]
-    )
+  :draw draw-pink-triangles)"]])
