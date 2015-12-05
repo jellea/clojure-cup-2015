985cd8c1266568172ec101be1d0e24723fb5ab18
Jelle Akkerman <jelle@wrte.io>
2015-12-05 18:45:06 +0100

    right editor props

--- src/clojure_cup_2015/editor.cljs
+++ src/clojure_cup_2015/editor.cljs
@@ -2,12 +2,18 @@
   (:require [reagent.core :as reagent :refer [atom]]
             [clojure-cup-2015.common :refer [config !state]]))
 
+(def opts {:matchBrackets true
+           :lineNumbers false
+           :autoCloseBrackets true
+           :theme "monokai"
+           :mode "clojure"})
+
 (defn cm-editor
   [props cm-opts]
   (reagent/create-class
    {:component-did-mount
     (fn [this]
-      (let [editor (.fromTextArea js/CodeMirror (reagent/dom-node this) (clj->js cm-opts))]
+      (let [editor (.fromTextArea js/CodeMirror (reagent/dom-node this) (clj->js (merge opts cm-opts)))]
         (.on editor "change" #((:on-change props) (.getValue %)))
         (reagent/set-state this {:editor editor})))
 
