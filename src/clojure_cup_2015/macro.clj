5d71fa1ba04e8138bdb784417123bccdbd54a860
Paulus Esterhazy <pesterhazy@gmail.com>
2015-12-06 13:33:57 +0100

    Make macro work again

--- src/clojure_cup_2015/macro.clj
+++ src/clojure_cup_2015/macro.clj
@@ -1,13 +1,6 @@
 (ns clojure-cup-2015.macro)
 
-#_(def out (clojure.java.io/writer "log.txt" :append true))
-
-(defn log [& args]
-  (.write out (str (clojure.string/join " " (map str args)) \newline))
-  (.flush out))
-
 (defn read-resource [n]
-  #_(log "Reading resource:" n)
   (some->> (str "code/" n ".cljs")
            clojure.java.io/resource
            clojure.java.io/file
